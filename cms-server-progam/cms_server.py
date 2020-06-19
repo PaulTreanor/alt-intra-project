82990fea3035cc7c3a3f042b4091f36c6c3e1e89
Paul Treanor <treanorpaul9@gmail.com>
2020-06-19 14:14:36 +0100

    cms front and back end

--- cms-server-progam/cms_server.py
+++ cms-server-progam/cms_server.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
 from forms import ConfigForm
 from forms import RequestForm
+import pickle
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] ='altintra'
+
+
+#hardcoding some variables because no front end 
+live_devices = ["device_5"]    
+device_config_details = {
+			"sensor_sensitivity": 7,
+			"amount_dispensed": 50
+						}
 
 config_result = {}
 request_result = {}
@@ -16,6 +26,7 @@ def config():
 	form = ConfigForm()
 	if form.is_submitted():
 		config_result = request.form
+		
 	return render_template('config.html', form=form)
 
 @app.route("/request", methods=['GET', 'POST'])
@@ -24,6 +35,14 @@ def request_data():
 	if form.is_submitted():
 		request_result = request.form
 	return render_template('request.html', form=form)
+
+
+#put new config into into a domain . 
+@app.route('/cms/device_config/<device_id>')
+def update_device_config(device_id):
+	return config_result  
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
